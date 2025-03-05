d5a598817607604563b8a0b39d4fc18a29935608
Francisco Macedo <macedo270701@gmail.com>
2025-03-05 12:01:24 -0300

    style(openROuteService): modifiquei a forma como o resultado da distancia era convertido para quilometros

--- src/services/openRouteService.ts
+++ src/services/openRouteService.ts
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import { config } from '../config/env';
-import { logger } from '../config/logger';
+import axios from "axios";
+import { config } from "../config/env";
+import { logger } from "../config/logger";
 
 interface Coordinates {
   latitude: number;
@@ -13,25 +13,28 @@ interface RouteResponse {
 }
 
 export class OpenRouteService {
-  static async getRoute(start: Coordinates, end: Coordinates): Promise<RouteResponse | null> {
+  static async getRoute(
+    start: Coordinates,
+    end: Coordinates
+  ): Promise<RouteResponse | null> {
     try {
       const response = await axios.get(config.OPEN_ROUTE_SERVICE_URL, {
         params: {
           api_key: config.OPEN_ROUTE_SERVICE_API_KEY,
           start: `${start.longitude},${start.latitude}`,
-          end: `${end.longitude},${end.latitude}`
-        }
+          end: `${end.longitude},${end.latitude}`,
+        },
       });
-      
+
       const route = response.data.features[0];
-      
+
       if (!route) {
         return null;
       }
-      
+
       return {
-        distance: (route.properties.summary.distance * 10^-3),
-        duration: (route.properties.summary.duration / 60) 
+        distance: route.properties.summary.distance / 1000,
+        duration: route.properties.summary.duration / 60,
       };
     } catch (error) {
       logger.error(`Erro ao consultar OpenRouteService: ${error}`);
