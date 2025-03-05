f7da890311cf9f060bd8a57d82d7e7216829cff4
Francisco Macedo <macedo270701@gmail.com>
2025-03-05 15:56:07 -0300

    style(seedScript): mantendo consistencia entre ordem das coordenadas

--- src/scripts/seedStores.ts
+++ src/scripts/seedStores.ts
@@ -1,76 +1,75 @@
-import mongoose from 'mongoose';
-import Store from '../models/store';
-import { config } from '../config/env';
+import mongoose from "mongoose";
+import Store from "../models/store";
+import { config } from "../config/env";
 
 const stores = [
   {
-    name: 'Loja Centro São Paulo',
-    cep: '01310-100',
-    street: 'Avenida Paulista',
-    number: '1000',
-    neighborhood: 'Bela Vista',
-    city: 'São Paulo',
-    state: 'SP',
+    name: "Loja Centro São Paulo",
+    cep: "01310-100",
+    street: "Avenida Paulista",
+    number: "1000",
+    neighborhood: "Bela Vista",
+    city: "São Paulo",
+    state: "SP",
     location: {
-      type: 'Point',
-      coordinates: [-46.6333, -23.5505] // [longitude, latitude]
-    }
+      type: "Point",
+      coordinates: [-46.6333, -23.5505], // [longitude, latitude]
+    },
   },
   {
-    name: 'Loja Recife I',
-    cep: '50870-005',
-    street: 'Avenida José Rufino',
-    number: '1407',
-    neighborhood: 'Areias',
-    city: 'Recife',
-    state: 'PE',
+    name: "Loja Recife I",
+    cep: "50870-005",
+    street: "Avenida José Rufino",
+    number: "1407",
+    neighborhood: "Areias",
+    city: "Recife",
+    state: "PE",
     location: {
-      type: 'Point',
-      coordinates: [-8.0913, -34.9355] // [longitude, latitude]
-    }
+      type: "Point",
+      coordinates: [-34.9355, -8.0913], // [longitude, latitude]
+    },
   },
   {
-    name: 'Loja Limoeiro I',
-    cep: '22250-040',
-    street: 'PE-050',
-    number: 'NA',
-    neighborhood: 'NA',
-    city: 'Limoeiro',
-    state: 'PE',
+    name: "Loja Limoeiro I",
+    cep: "22250-040",
+    street: "PE-050",
+    number: "NA",
+    neighborhood: "NA",
+    city: "Limoeiro",
+    state: "PE",
     location: {
-      type: 'Point',
-      coordinates: [-7.8983, -35.4281] // [longitude, latitude]
-    }
+      type: "Point",
+      coordinates: [-35.4281, -7.8983], // [longitude, latitude]
+    },
   },
   {
-    name: 'Loja Caruaru I',
-    cep: '22250-040',
-    street: 'Rua Ypiraflor',
-    number: 'NA',
-    neighborhood: 'Cortume',
-    city: 'Caruaru',
-    state: 'PE',
+    name: "Loja Caruaru I",
+    cep: "22250-040",
+    street: "Rua Ypiraflor",
+    number: "NA",
+    neighborhood: "Cortume",
+    city: "Caruaru",
+    state: "PE",
     location: {
-      type: 'Point',
-      coordinates: [-8.1973, -36.0292] // [longitude, latitude]
-    }
-  }
-  // Adicione mais lojas conforme necessário
+      type: "Point",
+      coordinates: [-36.0292, -8.1973], // [longitude, latitude]
+    },
+  },
 ];
 
 async function seedStores() {
   try {
     await mongoose.connect(config.MONGODB_URI);
-    
+
     await Store.deleteMany({});
-    
+
     await Store.insertMany(stores);
-    
-    console.log('Lojas inseridas com sucesso!');
-    
+
+    console.log("Lojas inseridas com sucesso!");
+
     await mongoose.connection.close();
   } catch (error) {
-    console.error('Erro ao inserir lojas:', error);
+    console.error("Erro ao inserir lojas:", error);
     process.exit(1);
   }
 }
