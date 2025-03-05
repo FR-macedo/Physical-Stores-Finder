526bc423bf59d72a63e7ad170aa8914a0418a5ee
Francisco Macedo <macedo270701@gmail.com>
2025-03-05 15:57:00 -0300

    style(logger): Colorizadno os logs

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -4,7 +4,8 @@ export const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
     winston.format.timestamp(),
-    winston.format.json()
+    winston.format.json(),
+    winston.format.colorize({ all: true })
   ),
   transports: [
     new winston.transports.Console(),
