98f9315651fb1efbab96f95566cd4280974e3646
Francisco Macedo <macedo270701@gmail.com>
2025-03-08 13:42:50 -0300

    style(database): edited logger string

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,9 +5,9 @@ import { logger } from './logger';
 export const configureDatabase = async (): Promise<void> => {
   try {
     await mongoose.connect(config.MONGODB_URI);
-    logger.info('Conexão com o MongoDB estabelecida com sucesso');
+    logger.info('Conexão com o banco de dados estabelecida com sucesso');
   } catch (error) {
-    logger.error('Erro ao conectar ao MongoDB:', error);
+    logger.error('Erro ao conectar ao com o banco de dados:', error);
     process.exit(1);
   }
 };
